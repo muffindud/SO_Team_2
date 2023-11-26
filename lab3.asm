6033427e22faf153535f0bbe81d16f05704709e4
buffaloss <beatricia.golban@isa.utm.md>
2023-11-26 14:34:30 +0200

    write second part to floppy

--- lab3.asm
+++ lab3.asm
@@ -26,8 +26,30 @@ start:
     mov bx, sector_1
     int 13h
 
+    ; Write sector 2
+    ; to 2281
+    mov ah, 03h
+    mov al, 0x1
+    mov ch, 0x3F
+    mov dh, 0x0
+    mov cl, 0xE
+    mov bx, sector_2
+    int 13h
+
+    ; to 2310
+    mov ah, 03h
+    mov al, 0x1
+    mov ch, 0x40
+    mov dh, 0x0
+    mov cl, 0x7
+    mov bx, sector_2
+    int 13h
+
     jmp $
 
 section .data
     sector_1 db "@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###@@@FAF-213 Corneliu Catlabuga###"
     times 0x200 - ($ - sector_1) db 0x00
+
+    sector_2 db "@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###@@@FAF-213 Beatricia Golban###"
+    times 0x200 - ($ - sector_2) db 0x00
